38ac64c8615fc018ccfeebcf9cad23d70f289609
김승빈 <note11@kakao.com>
2023-07-22 14:02:36 +0900

    Fix include dash(-) url not detected issue

--- lib/src/utils/regex.dart
+++ lib/src/utils/regex.dart
@@ -4,7 +4,7 @@ import 'package:linkfy_text/src/enum.dart';
 
 // url regex that accept https, http, www
 String urlRegExp =
-    r'((https?://)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))';
+    r'((https?://)?(www\.)?[a-zA-Z0-9@:%._\+~#=\-]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))';
 
 String hashtagRegExp = r'(#+[a-zA-Z0-9(_)]{1,})';
 
